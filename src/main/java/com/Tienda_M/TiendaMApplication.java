506eca5a0d444c08b6dd404df61ca92d4d32ed48
emilyherrera <emilyherrera@localhost>
2023-05-24 13:20:47 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/Tienda_M/TiendaMApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_M;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMApplication.class, args);
+	}
+
+}
